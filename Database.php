68e779a010d0e2f384f7d2bbf5752393e881e9ae
arghavan <arghavan.habibibibalani@gmail.com>
2025-08-10 12:32:51 +0330

    Show notes in index page
    On this page, all notes inside our database are displayed in a table, allowing us to add new notes and edit existing ones.

--- Database.php
+++ Database.php
@@ -10,7 +10,7 @@ $conn = mysqli_connect($serverName, $username, $password, $databaseName);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
   }
-  echo "Connected successfully";
+  //echo "Connected successfully";
 
   
   ?>
---
+++ index.php
@@ -0,0 +1,45 @@
+<?php
+include 'Database.php';
+
+$sql = "SELECT * FROM notes";
+$result = mysqli_query($conn, $sql);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My notes</title>
+</head>
+<body>
+    <h1>My Notes</h1>
+
+    <table border="1" cellpadding="5">
+        <tr>
+            <th>Title</th>
+            <th>Content</th>
+            <th>Creation Date</th>
+            <th>Edit note</th>
+        </tr>
+        <?php
+        if (mysqli_num_rows($result) > 0) {
+            while ($row = mysqli_fetch_assoc($result)) {
+                echo "<tr>";
+                echo "<td>" . htmlspecialchars($row['Title']) . "</td>";
+                echo "<td>" . htmlspecialchars($row['Body']) . "</td>";
+                echo "<td>" . htmlspecialchars($row['CreationDate']) . "</td>";
+                echo "<td><a href='Edit.php?id=" . $row['id_Notes'] . "'>Edit</a></td>";
+                
+                echo "</tr>";
+            }
+        } else {
+            echo "<tr><td colspan='3'>No notes found</td></tr>";
+        }
+        ?>
+    </table>
+    <br><br>
+    <button onclick="window.location.href='Add.php'">Add New</button>
+    <button onclick="window.location.href='index.php'">Home</button>
+</body>
+</html>
