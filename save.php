a613c5a858f71aae514c562dcf644352a1002fc0
arghavan <arghavan.habibibibalani@gmail.com>
2025-08-10 09:43:41 +0330

    Save forms
    Using this, we can save and view our database in phpMyAdmin. To identify errors, I checked the A_I column in the id (primary key) to ensure the auto-incremented ID is provided for each entry.

---
+++ save.php
@@ -0,0 +1,18 @@
+<?php
+include 'Database.php';
+
+$TITLE = $_POST['title'];
+$CONTENT = $_POST['content'];
+$date = date("Y-m-d H:i:s");
+
+$sql = "INSERT INTO notes (Title, Body, CreationDate) VALUES ('$TITLE', '$CONTENT', '$date')";   
+
+if (mysqli_query($conn, $sql)) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+
+mysqli_close($conn);
+?>
