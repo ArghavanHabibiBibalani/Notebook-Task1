62609df3a12ffa6bfe79f05fdc8c515fe5cd748a
arghavan <arghavan.habibibibalani@gmail.com>
2025-08-10 09:27:31 +0330

    Add form
    We can now add new notes using this page.

---
+++ Add.php
@@ -0,0 +1,22 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add note</title>
+</head>
+<body>
+    <h1>Add Note</h1>
+
+    <form action="save.php" method="post">
+        <label for="title">Title:</label><br>
+        <input type="text" placeholder="title" id="title" name="title" required><br><br>
+
+        <textarea id="content" placeholder="type your note HERE." name="content" rows="4" cols="50" required></textarea><br><br>
+
+        <button type="submit">Submit</button>
+</body>
+</html>
